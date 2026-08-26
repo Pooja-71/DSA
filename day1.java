4ecf8dc9141fe45b320983eea08ca5da53bf1ce6
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-26 22:32:44 +0530

    Print an Array

--- day1.java
+++ day1.java
@@ -1,5 +1,16 @@
-public class Main {
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, DSA!");
+//     }
+// }
+
+public class day1 {
     public static void main(String[] args) {
-        System.out.println("Hello, DSA!");
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
