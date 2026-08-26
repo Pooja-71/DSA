e51547cdefb5b645aa33473f160956f1ab6cef48
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-26 22:37:28 +0530

    Find the Largest Element

--- day1.java
+++ day1.java
@@ -4,13 +4,30 @@
 //     }
 // }
 
+// public class day1 {
+//     public static void main(String[] args) {
+
+//         int[] arr = {10, 20, 30, 40, 50};
+
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
 public class day1 {
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] arr = {10, 45, 23, 67, 12};
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
+
+        System.out.println("Largest = " + max);
     }
 }
