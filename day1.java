b2c9ba9e15762810fe6d1d36639a5f008876dd1c
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-30 13:06:36 +0530

    Sum of All Array Elements

--- day1.java
+++ day1.java
@@ -15,19 +15,34 @@
 //     }
 // }
 
+// public class day1 {
+//     public static void main(String[] args) {
+
+//         int[] arr = {10, 45, 23, 67, 12};
+
+//         int max = arr[0];
+
+//         for (int i = 1; i < arr.length; i++) {
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+
+//         System.out.println("Largest = " + max);
+//     }
+// }
+
 public class day1 {
     public static void main(String[] args) {
 
-        int[] arr = {10, 45, 23, 67, 12};
+        int[] arr = {10, 20, 30, 40, 50};
 
-        int max = arr[0];
+        int sum = 0;
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
         }
 
-        System.out.println("Largest = " + max);
+        System.out.println("Sum = " + sum);
     }
 }
