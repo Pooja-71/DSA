035070d009915cbe485f3964fb94726982711c5f
Pooja-71 <poojakumari8546246@gmail.com>
2026-09-03 14:55:08 +0530

    Frequency of Each Element

--- day1.java
+++ day1.java
@@ -240,23 +240,55 @@
 //     }
 // }
 
+// public class day1 {
+//     public static void main(String[] args) {
+
+//         int num = 121;
+//         int original = num;
+//         int reverse = 0;
+
+//         while (num != 0) {
+//             int digit = num % 10;
+//             reverse = reverse * 10 + digit;
+//             num = num / 10;
+//         }
+
+//         if (original == reverse) {
+//             System.out.println("Palindrome");
+//         } else {
+//             System.out.println("Not Palindrome");
+//         }
+//     }
+// }
+
 public class day1 {
     public static void main(String[] args) {
 
-        int num = 121;
-        int original = num;
-        int reverse = 0;
+        int[] arr = {10, 20, 10, 30, 20, 10};
 
-        while (num != 0) {
-            int digit = num % 10;
-            reverse = reverse * 10 + digit;
-            num = num / 10;
-        }
+        for (int i = 0; i < arr.length; i++) {
 
-        if (original == reverse) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
+            boolean alreadyCounted = false;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    alreadyCounted = true;
+                    break;
+                }
+            }
+
+            if (!alreadyCounted) {
+
+                int count = 0;
+
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[i] == arr[j]) {
+                        count++;
+                    }
+                }
+
+                System.out.println(arr[i] + " occurs " + count + " times");
+            }
         }
     }
 }
