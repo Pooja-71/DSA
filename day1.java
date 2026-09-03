14dd479581169f9f41debdd083c8b31d5a442ae8
Pooja-71 <poojakumari8546246@gmail.com>
2026-09-03 14:52:54 +0530

    Check Palindrome Number

--- day1.java
+++ day1.java
@@ -224,10 +224,27 @@
 //         }
 //     }
 // }
+// public class day1 {
+//     public static void main(String[] args) {
+
+//         int num = 12345;
+//         int reverse = 0;
+
+//         while (num != 0) {
+//             int digit = num % 10;
+//             reverse = reverse * 10 + digit;
+//             num = num / 10;
+//         }
+
+//         System.out.println("Reverse = " + reverse);
+//     }
+// }
+
 public class day1 {
     public static void main(String[] args) {
 
-        int num = 12345;
+        int num = 121;
+        int original = num;
         int reverse = 0;
 
         while (num != 0) {
@@ -236,6 +253,10 @@ public class day1 {
             num = num / 10;
         }
 
-        System.out.println("Reverse = " + reverse);
+        if (original == reverse) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
